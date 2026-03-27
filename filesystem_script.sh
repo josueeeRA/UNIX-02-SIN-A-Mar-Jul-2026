6c4cee2ed6cc2be01ceb1ffffa02e8a6fc8da0b5
josueeeRA <josueandresra2006@gmail.com>
2026-03-27 17:27:17 +0000

    -F and-i

--- filesystem_script.sh
+++ filesystem_script.sh
@@ -3,3 +3,7 @@ $ls #List directories on the /dev directory
 $history #To view all the commands I run in the terminal 
 cd /proc #To change to /proc
 /proc $cat cpuinfo #Views specsof the virtual cpu
+pwd #print working directory
+cd / #To enter in the root directory
+ls -F # For view the indicator of the files
+ls -i #Fow view the inodes 
