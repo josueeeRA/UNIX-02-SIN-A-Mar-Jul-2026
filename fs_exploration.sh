6f56770e53d50e3a025496e9da8108f9e0574d90
josueeeRA <josueandresra2006@gmail.com>
2026-04-02 12:56:09 +0000

    commands update, upgrade and install

---
+++ fs_exploration.sh
@@ -0,0 +1,11 @@
+# Use cd  / to move to the root directory 
+cd /
+# Use this command to list the indicators in the fyles 
+ls -F
+# Update the local libraries 
+sudo apt update
+# Upgrade the local libraries 
+sudo apt upgrade 
+# For install parted
+sudo apt install parted
+#
