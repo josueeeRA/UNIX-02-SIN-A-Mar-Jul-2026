88926938c1d6ae2b30b0ffc450e6f835249e807c
josueeeRA <josueandresra2006@gmail.com>
2026-04-24 16:48:03 +0000

    Fix problema 3: permisos numericos 600 en sensores.log

---
+++ HISTORY.sh
@@ -0,0 +1,11 @@
+13  cd registros/
+   14  ls
+   15  cd ..
+   16  ls
+   17  mkdir plantillas
+   18  ls
+   19  git add .
+   20  git commit -m "Fix problema 1: estructura de directorios corregida"
+   21  ls
+   22  git push 
+   23  history
