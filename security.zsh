3115dc017f0f798eba65e4651c536d811a1dbdc0
josueeeRA <josueandresra2006@gmail.com>
2026-05-12 17:09:27 +0000

    feat: new file and notes of umask 077

--- security.zsh
+++ security.zsh
@@ -9,3 +9,6 @@ umask 027
 touch archivo3 # Create a new file 
 mkdir directorio3 # Create a new directory 
 ls -l # List the directory in a long way 
+umask 077 # To rest the number 077 to the permissions of the new file
+touch secreto.txt # Create a new file 
+ls -l
