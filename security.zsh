4089f21a33c7e71d313c59f3169c7fd99c9fd961
josueeeRA <josueandresra2006@gmail.com>
2026-05-12 17:07:21 +0000

    docs: Fix umask problem and notes

---
+++ security.zsh
@@ -0,0 +1,11 @@
+apt-get upate # Update the packages to the last versions 
+apt-get upgrade  # Apply the changes 
+apt-get install acl # Install acl
+# Commands to fix the problem of the umask 
+chown -R $(whoami) . 
+setfacl -bnR .
+# Now mae the changes and do umask 027 again
+umask 027
+touch archivo3 # Create a new file 
+mkdir directorio3 # Create a new directory 
+ls -l # List the directory in a long way 
