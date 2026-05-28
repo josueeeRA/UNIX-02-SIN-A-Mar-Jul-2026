ef154c397218bd4633ab3911d7f0a3982a2917b7
josueeeRA <josueandresra2006@gmail.com>
2026-05-28 12:55:09 +0000

    wip: ls -la ~/antes_de_newgrp.txt aechive

--- security2.sh
+++ security2.sh
@@ -6,3 +6,9 @@ id -gn
 touch ~/test_group_heredado.txt
 # List in a long for the archive 
 ls -la ~/test_group_heredado.txt
+# Show the main group with a message
+echo "Grupo actual $(id -gn)"
+#create a archive with this name
+touch ~/antes_de_newgrp.txt
+# List in a long way the archive
+ls -la ~/antes_de_newgrp.txt
