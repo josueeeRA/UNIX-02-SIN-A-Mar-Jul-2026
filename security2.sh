7e4a631546c705845279da4deafd5eab2204dd05
josueeeRA <josueandresra2006@gmail.com>
2026-05-28 12:49:56 +0000

    wip: test_group_heredado.txt

---
+++ security2.sh
@@ -0,0 +1,8 @@
+# Show the user who im
+id
+# Only show the main user 
+id -gn
+# Create a archive with this name 
+touch ~/test_group_heredado.txt
+# List in a long for the archive 
+ls -la ~/test_group_heredado.txt
