2c0b8cf27f2237201b4f13fa299be800d28daa5f
josueeeRA <josueandresra2006@gmail.com>
2026-05-28 14:04:14 +0000

    wip: restring group

--- security2.sh
+++ security2.sh
@@ -12,3 +12,23 @@ echo "Grupo actual $(id -gn)"
 touch ~/antes_de_newgrp.txt
 # List in a long way the archive
 ls -la ~/antes_de_newgrp.txt
+# Create a temporary newgroup
+newgrp desarrolladores
+# Show the user 
+id -gn
+# Create a new archive
+touch ~/dentro_de_newgrp.txt
+# List in the long way 
+ls -la ~/dentro_de_newgrp.txt
+# make a directory
+mkdir -p ~/proyecto_dev/src
+# show this message in the terminal 
+echo "Grupo restaurado: $(id -gn)"
+# add a group
+groupadd grupo_restringido
+# set a password for the group
+gpasswd grupo_restringido
+# create a temporary group
+newgrp grupo_restringido
+# show the user
+id -gn
