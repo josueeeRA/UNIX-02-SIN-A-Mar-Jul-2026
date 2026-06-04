d14b654f7aa014f199a1d777fa81b5f074508e67
josueeeRA <josueandresra2006@gmail.com>
2026-06-04 12:57:06 +0000

    wip: ps, df and bash -r, -n commands

--- blackhatbash1.sh
+++ blackhatbash1.sh
@@ -1,6 +1,17 @@
+# !/usr/bin/env bash 
 bash --version
 env
 echo ${SHELL}
 echo ${RANDOM}
 echo ${UID}
 echo ${OSTYPE}
+# man ls 
+ps -e -f
+df -h
+bash -r blackhatbash1.sh
+# bash -n blackhatbash1.s
+#!/bin/bash
+set -x
+
+--snip--
+set +x
