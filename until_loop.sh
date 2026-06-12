a1d6b2e4b9ac1db779a5746fb7ba4ef136469976
josueeeRA <josueandresra2006@gmail.com>
2026-06-12 16:50:19 +0000

    until loop comments

--- until_loop.sh
+++ until_loop.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-FILE="output11.txt"
-touch "${FILE}"
-until [[ -s "${FILE}" ]]; do
+FILE="output11.txt" # Create a global enviroment variable 
+touch "${FILE}" # Create the file
+until [[ -s "${FILE}" ]]; do # Do the loop
  echo "${FILE} is empty..."
  echo "Checking again in 2 seconds..."
 sleep 2
 done
- echo "${FILE} appears to have some content in it!"
+ echo "${FILE} appears to have some content in it!" # Check if the archive exist
