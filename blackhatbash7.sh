32f31699956ff413bbeadf6bccffe223208c65a8
josueeeRA <josueandresra2006@gmail.com>
2026-06-18 13:09:32 +0000

    grep comannds withj awk

--- blackhatbash7.sh
+++ blackhatbash7.sh
@@ -5,7 +5,12 @@ grep -v "35.237.4.214" log.txt
 grep -o "35.237.4.214" log.txt
 awk '{print $1}' log.txt
 awk '{print $1,$2,$3}' log.txt
-awk '{print $1}' log.t
-awk '{print $1}' log.t
+awk '{print $2}' log.t
+awk '{print $3}' log.t
 awk '{print $1,$NF}' log.txt
 awk -F',' '{print $1}' example_csv.txt
+awk 'head < 10' log.txt
+awk 'NR < 10' log.txt
+grep "42.236.10.117" log.txt
+awk '{print $7}' 
+grep "42.236.10.117" log.txt | awk '{print $7}''
