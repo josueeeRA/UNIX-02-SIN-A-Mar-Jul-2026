d771ca1b390164424b7b0ecba4457305db69bc07
josueeeRA <josueandresra2006@gmail.com>
2026-06-19 16:42:34 +0000

    blackhatbash7.sh commands

--- blackhatbash7.sh
+++ blackhatbash7.sh
@@ -14,3 +14,8 @@ awk 'NR < 10' log.txt
 grep "42.236.10.117" log.txt
 awk '{print $7}' 
 grep "42.236.10.117" log.txt | awk '{print $7}''
+sed 's/Mozilla/Godzilla/g' log.txt
+grep  'Mozilla' log.txt
+grep 'Godzilla' log.txt
+sed 's/Mozilla/Godzilla/g' log.txt > newlog.txt
+grep 'Godzilla' log.txt
